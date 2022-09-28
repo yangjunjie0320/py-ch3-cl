ac5b377862324c580cf2fbab6db049619e278e92
Junjie Yang <yangjunjie0320@gmail.com>
2022-09-27 23:37:56 -0700

    update

--- work/step_-26/main.py
+++ work/step_-26/main.py
@@ -25,35 +25,35 @@ def main(xyz_file, basis="631g*", xc="b3lyp", output="ene.out"):
 
     ene_mf = mf.e_tot
 
-    tda = pyscf.tdscf.TDA(mf)
-    tda.nstates = 5
-    tda.max_space = 8
-    tda.max_cycle = 2000
-    tda.kernel()
-
-    con_tda = tda.converged
-    ene_tda = tda.e[:5]
-    dip_tda = tda.transition_dipole()[:5]
-
-    rpa = pyscf.tdscf.RPA(mf)
-    rpa.nstates = 5
-    rpa.max_space = 8
-    rpa.max_cycle = 2000
-    rpa.kernel()
-
-    con_rpa = rpa.converged
-    ene_rpa = rpa.e[:5]
-    dip_rpa = rpa.transition_dipole()[:5]
-
     with open(output, "w") as f:
+        tda = pyscf.tdscf.TDA(mf)
+        tda.nstates = 5
+        tda.max_space = 8
+        tda.max_cycle = 2000
+        tda.kernel()
+
+        con_tda = tda.converged
+        ene_tda = tda.e[:5]
+        dip_tda = tda.transition_dipole()[:5]
+
         f.write(f"RKS energy: {ene_mf: 12.8f}\n")
         f.write("TDA results:\n")
         for i, (con, ene, dip) in enumerate(zip(con_tda, ene_tda, dip_tda)):
             f.write(f" {i:2d} {con} {ene: 12.8f} {dip[0]: 12.8f} {dip[1]: 12.8f} {dip[2]: 12.8f}\n")
+
+        # rpa = pyscf.tdscf.RPA(mf)
+        # rpa.nstates = 5
+        # rpa.max_space = 8
+        # rpa.max_cycle = 2000
+        # rpa.kernel()
+
+        # con_rpa = rpa.converged
+        # ene_rpa = rpa.e[:5]
+        # dip_rpa = rpa.transition_dipole()[:5]
         
-        f.write("\nRPA results:\n")
-        for i, (con, ene, dip) in enumerate(zip(con_rpa, ene_rpa, dip_rpa)):
-            f.write(f" {i:2d} {con} {ene: 12.8f} {dip[0]: 12.8f} {dip[1]: 12.8f} {dip[2]: 12.8f}\n")
+        # f.write("\nRPA results:\n")
+        # for i, (con, ene, dip) in enumerate(zip(con_rpa, ene_rpa, dip_rpa)):
+        #     f.write(f" {i:2d} {con} {ene: 12.8f} {dip[0]: 12.8f} {dip[1]: 12.8f} {dip[2]: 12.8f}\n")
 
 if __name__ == "__main__":
     xyz = sys.argv[1]
